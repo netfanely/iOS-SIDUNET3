4897070b3f633c4348b2ca747fb6f40172b246e5
netfanely <fanely@gmail.com>
2019-04-22 16:04:06 -0500

    navegaçao principalviewcontroler

---
+++ iOS-SIDUNET3/Controller/Home/MainViewController.swift
@@ -0,0 +1,22 @@
+//
+//  MainViewController.swift
+//  iOS-SIDUNET3
+//
+//  Created by Fanely on 4/22/19.
+//  Copyright © 2019 Net Fanely. All rights reserved.
+//
+
+//import Foundation
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func regresarInicio(segue: UIStoryboardSegue) {
+        //DEJAR VACIO
+    }
+}
---
+++ iOS-SIDUNET3/Controller/Home/PrincipalViewController.swift
@@ -0,0 +1,27 @@
+//
+//  PrincipalViewController.swift
+//  iOS-SIDUNET3
+//
+//  Created by Fanely on 4/22/19.
+//  Copyright © 2019 Net Fanely. All rights reserved.
+//
+
+//import Foundation
+import UIKit
+
+ 
+class PrincipalViewController: UIViewController {
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            
+            // Do any additional setup after loading the view.
+        }
+        
+        @IBAction func regresar(_ sender: UIButton) {
+            self.dismiss(animated: true, completion: nil)
+        }
+        
+        
+    
+}
