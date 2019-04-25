a15cf1fcc05f53ddd95b4a59e938e3f18eca6c17
netfanely <fanely@gmail.com>
2019-04-25 16:31:42 -0500

    salvar mostrar excluir entidad Clientes

--- iOS-SIDUNET3/Controller/Home/ClienteViewController.swift
+++ iOS-SIDUNET3/Controller/Home/ClienteViewController.swift
@@ -7,19 +7,97 @@
 //
 
 import UIKit
-
+import CoreData
 class ClienteViewController: UIViewController {
-
+    
+    @IBOutlet weak var codText: UITextField!
+    @IBOutlet weak var nomeText: UITextField!
+    @IBOutlet weak var emailText: UITextField!
+    @IBOutlet weak var enderecoText: UITextField!
+    @IBOutlet weak var barrioText: UITextField!
+    @IBOutlet weak var telefText: UITextField!
+    @IBOutlet weak var mphoneText: UITextField!
+    @IBOutlet weak var obsText: UITextField!
+    
+    func conexion()->NSManagedObjectContext{
+        let delegate = UIApplication.shared.delegate as! AppDelegate
+        return delegate.persistentContainer.viewContext
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
+    
+    
+    @IBAction func guardar(_ sender: UIButton) {
+        let contexto = conexion()
+        let entidadCliente = NSEntityDescription.insertNewObject(forEntityName: "Clientes", into: contexto) as! Clientes
+        
+        guard let codigo = Int16(codText.text!) else { return }
+        guard let telefono1 = Int32(telefText.text!) else { return }
+        guard let telefono2 = Int32(mphoneText.text!) else { return }
+        
+        entidadCliente.nomeCli=nomeText.text
+        entidadCliente.email=emailText.text
+        entidadCliente.endereco=enderecoText.text
+        entidadCliente.barrio=barrioText.text
+        entidadCliente.obs=obsText.text
+        entidadCliente.codCli = codigo
+        entidadCliente.telefone = telefono1
+        entidadCliente.mobilePhone = telefono2
+        
+        do {
+            try contexto.save()
+            print("salvó")
+            
+        }catch let error as NSError {
+            print("Erro ao salvar", error.localizedDescription)
+        }
+    
+    }
+    
+    @IBAction func mostrar(_ sender: UIButton) {
+        let contexto = conexion()
+        let fetchRequest : NSFetchRequest<Clientes> = Clientes.fetchRequest()
+        do{
+            let resultados = try contexto.fetch(fetchRequest)
+            print("numero de registros: \(resultados.count)")
+            for res in resultados as [NSManagedObject]{
+                let nome = res.value(forKey:"nomeCli")
+                let email = res.value(forKey: "email")
+                let endereco = res.value(forKey: "endereco")
+                let barrio = res.value(forKey: "barrio")
+                let obs = res.value(forKey: "obs")
+                let codigo = res.value(forKey: "codCli")
+                let telefono1 = res.value(forKey: "telefone ")
+                let telefono2 = res.value(forKey: "mobilePhone")
+                
+                print("Nombre: \(nome!) E-mail: \(email!) Direccion: \(endereco!) Bairro: \(barrio!) Observacao: \(obs!) Codigo: \(codigo!) Celular: \(telefono1!) Celular2: \(telefono2!)")
+            }
+        }catch let error as NSError{
+            print("Erro al mostrar",error.localizedDescription)
+            
+        }
+    }
+
     @IBAction func regresar(_ sender: UIButton) {
         //self.dismiss(animated: true, completion: nil)
         navigationController?.popViewController(animated: true)
     }
     
+    @IBAction func borrar(_ sender: UIButton){
+        let contexto = conexion()
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Alumnos")
+        let borrar = NSBatchDeleteRequest(fetchRequest: fetchRequest)
+        do {
+            try contexto.execute(borrar)
+            print("Registros borrados")
+        } catch let error as NSError {
+            print("Error al borrar", error.localizedDescription)
+        }
+    }
 
 }
